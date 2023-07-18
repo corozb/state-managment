057bb394488a82abe75bd1b4ce66f2ff4d0f90f1
Cristian Orozco <cristian.orozco@globant.com>
2023-07-18 18:36:45 -0500

    redux toolkit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import { setAccessToken } from '../redux/slices/auth'
+import { useAppDispatch, useAppSelector } from '../hooks'
+
 function Home() {
-  return <div>Home App</div>
+  const { auth } = useAppSelector((state) => state)
+  const dispatch = useAppDispatch()
+  console.log(auth.accessToken)
+
+  const handleLogin = () => {
+    dispatch(setAccessToken('123456'))
+  }
+  return (
+    <>
+      <div>Home App</div>
+      <button onClick={handleLogin}>Login</button>
+    </>
+  )
 }
 
 export default Home
