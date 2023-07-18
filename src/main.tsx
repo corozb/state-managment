87401418e66391f9da0f20329c54d95e5a54884b
Cristian Orozco <cristian.orozco@globant.com>
2023-07-18 18:57:05 -0500

    redux toolkit persistor

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
-import store from './redux/store'
+import store, { persistor } from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './slices/auth'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import authReducer from './slices/auth'
+
+const persistAuthConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['accessToken'],
+}
 
 const store = configureStore({
   reducer: {
-    auth: authSlice,
+    auth: persistReducer<ReturnType<typeof authReducer>>(persistAuthConfig, authReducer),
   },
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+export const persistor = persistStore(store)
 export default store
